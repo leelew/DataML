3d0b57e64b7a237e503eba2d91721663f621f6e1
leelew <lilu83@mail.sysu.edu.cn>
2022-09-30 20:31:16 +0800

    Original codes

---
+++ run.sh
@@ -0,0 +1,9 @@
+python3 src/main.py --et_product          "GLEAM_hybrid" \
+	        --begin_year          2003 \
+		--end_year            2004 \
+		--et_begin_year       2003 \
+		--et_end_year         2019 \
+		--temporal_resolution "1D" \
+		--spatial_resolution  "0p25" \
+		--num_leaves          50 \
+		--n_estimators        100 \
